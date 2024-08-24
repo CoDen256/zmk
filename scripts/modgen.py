a18eb5082bf4443e7da8a262198b031f81d99df0
Denys Chernyshov <den.blackshov@gmail.com>
2024-08-24 12:59:56 +0200

    add mod gen

--- scripts/modgen.py
+++ scripts/modgen.py
@@ -1,6 +1,88 @@
 
-map = {
-    "G" : "X",
+
+modmap = {
+    "lalt" : "MOD_LALT",
+    "ralt" : "MOD_RALT",
+    "lctrl" : "MOD_LCTL",
+    "rctrl" : "MOD_RCTL",
+    "lgui" : "MOD_LGUI",
+    "rgui" : "MOD_RGUI",
+    "lshift" : "MOD_LSFT",
+    "rshift" : "MOD_RSFT",
+}
+
+class Morph:
+    def __init__(self, parent, prefix, default, mods, modified, keep=False, postfix = ""):
+        self.parent = parent
+        self.prefix = prefix
+        self.postfix = postfix if postfix else f"{'_'.join(mods)}"
+        self.default = default
+        self.mods = mods
+        self.modified = modified
+        self.keep = keep
+
+    def name(self):
+        return f"{self.parent}_{self.prefix}_{self.postfix}"
+    def compile(self):
+        label = self.name()
+        m = list(map(lambda x: modmap[x], self.mods))
+        joined = "|".join(m)
+        mods = f"<({joined})>"
+        keep_mods = f"keep-mods = {mods};" if self.keep else ""
+        return f"""
+{label}:{label}{{
+compatible = "zmk,behavior-mod-morph";
+#binding-cells = <0>;
+bindings = <{self.default}>, <{self.modified}>;
+label = "{label.upper()}";
+mods = {mods};
+{keep_mods}
+}};"""
+
+
+class Map:
+    def __init__(self, default, mapping):
+        self.default = default
+        self.mapping = mapping
+    def generate(self):
+        sinks = []
+        links = []
+        prev = self.kp(self.default)
+        for (key, value) in self.mapping.items():
+            mods_complement = set(modmap.keys()) - {key}
+            sink = Morph(self.default, "sink",
+                         self.kp(value), mods_complement,
+                         self.kp(self.default), True, key)
+            sinks.append(sink)
+            link = Morph(self.default, "link",
+                         prev, [key],
+                         self.gen_ref(sink))
+            links.append(link)
+            prev = self.gen_ref(link)
+
+        return prev,links+sinks
+
+
+    def kp(self, key):
+        return f"&kp {key}"
+
+    def gen_ref(self, morph):
+        return f"&{morph.name()}"
+    def compile(self):
+        c = ""
+        prev, r = self.generate()
+        for i in r:
+            c += i.compile() + "\n"
+        return prev, c
+
+a = Map("C", {"lshift" : "LC(Z)", "rctrl": "RC(F14)"})
+r = a.generate()
+(prev,gen) = a.compile()
+print(gen)
+print(prev)
+
+mapp = {
+    "" : "X",
     "H" : "V",
     "I" : "S",
     "U" : "Z",
@@ -40,6 +122,6 @@ label = "{key}LCTRL";
 mods = <(MOD_LSFT|MOD_LALT|MOD_LGUI|MOD_RCTL|MOD_RSFT|MOD_RALT|MOD_RGUI)>;
 keep-mods = <(MOD_RCTL|MOD_LGUI|MOD_LALT|MOD_LSFT|MOD_RALT|MOD_RSFT|MOD_RGUI)>;}};
 '''
-    print(res + "\n")
-for (k, v) in map.items():
-    gen(k, v)
+    # print(res + "\n")
+# for (k, v) in mapp.items():
+#     gen(k, v)
