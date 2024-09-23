ccc1863bab94d956bc1116039a920620a8843b4f
Denys Chernyshov <den.blackshov@gmail.com>
2024-09-23 18:50:28 +0200

    fix some mods

--- shortcuts/create_sym.ps1
+++ shortcuts/create_sym.ps1
@@ -1,2 +1,3 @@
-New-Item -ItemType SymbolicLink -Force -Path "C:\Users\denbl\AppData\Roaming\JetBrains\IntelliJIdea2024.2\keymaps\keymap.xml" -Target ".\shortcuts\keymap.xml"
-New-Item -ItemType SymbolicLink -Force -Path "C:\Users\denbl\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\layout.ahk" -Target ".\shortcuts\layout.ahk"
+New-Item -ItemType SymbolicLink -Force -Path "~\AppData\Roaming\JetBrains\IntelliJIdea2024.2\keymaps\keymap.xml" -Target ".\shortcuts\keymap.xml"
+New-Item -ItemType SymbolicLink -Force -Path "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\layout.ahk" -Target ".\shortcuts\layout.ahk"
+New-Item -ItemType SymbolicLink -Force -Path "~\.yazi\keymap.toml" -Target ".\shortcuts\keymap.toml"
